3d64c65a98e80218ae6c4d0da7780098d5421f0f
Sharlotte <aaa9810321@gmail.com>
2024-07-31 09:42:09 +0900

    add some default values

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,72 @@ import DropZone from "./DropZone";
 import { motion } from "framer-motion";
 import ColorInput from "./ColorInput";
 import JSZip from "jszip";
+import { Midi } from "@tonejs/midi";
+import MidiFile from "midi-file";
 
-function App() {
+const defaultJJMData: JJMData = {
+  displayName: "",
+  songAuthor: "",
+  midiAuthor: "",
+  releaseYear: "",
+  genre: "",
+  difficulty: 2,
+  description: "",
+  coverColor: "#000000",
+  coverColor2: "#000000",
+  channelHidden: [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ],
+  channelSuggestedInstrument: [0, 0, 0, 0, 0, 0, 0, 0, 0b0010, 0, 0, 0, 0, 0, 0, 0],
+  targetAccuracy: [1, 1, 1, 1],
+  midiFileName: "",
+  revision: 2,
+};
+
+export default function App() {
   const [file, setFile] = useState<File | null>(null);
-  const [data, setData] = useState<JJMData | undefined>();
+  const [data, setData] = useState<JJMData>(defaultJJMData);
   const [image, setImage] = useState<File | null>(null);
 
+  const handleFile = (file: File) => {
+    setFile(file);
+    setData((prev) => ({ ...prev, midiFileName: file.name }));
+
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      const midi = new Midi(e.target!.result as ArrayBuffer);
+      const midifile = MidiFile.parseMidi(new Uint8Array(e.target!.result as ArrayBuffer));
+      midifile.tracks.forEach((tracks) =>
+        tracks.forEach((track) => {
+          switch (track.type) {
+            case "copyrightNotice":
+              console.log("COPYRIGHT", track);
+              setData((prev) => ({ ...prev, description: prev.description + track.text }));
+              break;
+          }
+        })
+      );
+      console.log(midi);
+    };
+    reader.readAsArrayBuffer(file);
+  };
   const handleFormChange = (e: React.ChangeEvent<HTMLFormElement>) => {
-    const formData = new FormData(e.currentTarget);
-    const data = Object.fromEntries(formData.entries()) as unknown as JJMData;
-    console.log(data);
-    data.channelHidden = Array.from({ length: 16 }, () => false) as JJMData["channelHidden"];
-    data.channelSuggestedInstrument = Array.from({ length: 16 }, () => 0) as JJMData["channelSuggestedInstrument"];
-    data.channelSuggestedInstrument[9] = 0b0010;
-    data.targetAccuracy = [1, 1, 1, 1];
-    data.midiFileName = file!.name;
-    data.revision = 2;
-    setData(data);
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
   const handleSubmit = () => {
     if (!file || !data || !image) return;
@@ -30,15 +79,7 @@ function App() {
     zip.file("song.json", encodeUTF16LE(JSON.stringify(data)));
     zip.file("cover." + image.name.split(".").at(-1), image, { base64: true });
     zip.generateAsync({ type: "blob" }).then(function (content) {
-      const url = window.URL.createObjectURL(content);
-      const a = document.createElement("a");
-      document.body.appendChild(a);
-      a.style.cssText = "display: none";
-      a.href = url;
-      a.download = data.displayName + ".jjm";
-      a.click();
-      window.URL.revokeObjectURL(url);
-      a.remove();
+      downloadBlob(content, data.displayName + ".jjm");
     });
   };
 
@@ -47,41 +88,65 @@ function App() {
       <Heading align="center" mb="2">
         MIDI to JJM
       </Heading>
-      <DropZone exts={[".mid", ".midi"]} handleFile={(f) => setFile(f)} label="drop a midi file here or " />
+      <DropZone exts={[".mid", ".midi"]} handleFile={handleFile} label="drop a midi file here or " />
       {file && (
         <Box asChild mt="4">
           <motion.form initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} onChange={handleFormChange}>
             <Flex direction="column" gap="2">
-              <TextField.Root placeholder="Display Name" name="displayName" required />
+              <TextField.Root placeholder="Display Name" name="displayName" value={data.displayName} required />
               <Flex gap="2">
-                <TextField.Root placeholder="Song Author" name="songAuthor" style={{ flex: 1 }} required />
-                <TextField.Root placeholder="MIDI Arragner" name="midiAuthor" style={{ flex: 1 }} required />
-              </Flex>
-              <Flex gap="2">
-                <TextField.Root placeholder="Release Year" name="releaseYear" style={{ flex: 1 }} required />
-                <TextField.Root placeholder="Genre" name="genre" style={{ flex: 1 }} required />
                 <TextField.Root
-                  placeholder="Difficulty"
-                  name="difficulty"
-                  type="number"
-                  defaultValue="2"
+                  placeholder="Song Author"
+                  name="songAuthor"
+                  value={data.songAuthor}
+                  style={{ flex: 1 }}
+                  required
+                />
+                <TextField.Root
+                  placeholder="MIDI Arragner"
+                  name="midiAuthor"
+                  value={data.midiAuthor}
                   style={{ flex: 1 }}
                   required
                 />
               </Flex>
-              <TextArea placeholder="Description" name="description" required />
+              <Flex gap="2">
+                <TextField.Root
+                  placeholder="Release Year"
+                  name="releaseYear"
+                  value={data.releaseYear}
+                  style={{ flex: 1 }}
+                  required
+                />
+                <TextField.Root placeholder="Genre" name="genre" value={data.genre} style={{ flex: 1 }} required />
+                <TextField.Root
+                  placeholder="Difficulty"
+                  name="difficulty"
+                  type="number"
+                  value={data.difficulty}
+                  style={{ flex: 1 }}
+                  required
+                />
+              </Flex>
+              <TextArea
+                placeholder="Description"
+                name="description"
+                value={data.description}
+                required
+                resize="vertical"
+              />
               <Flex gap="2">
                 <ColorInput
                   placeholder="cover color 1"
-                  name="coverColor1"
-                  defaultValue="#000000"
+                  name="coverColor"
+                  value={data.coverColor}
                   style={{ flex: 1 }}
                   required
                 />
                 <ColorInput
                   placeholder="cover color 2"
                   name="coverColor2"
-                  defaultValue="#000000"
+                  value={data.coverColor2}
                   style={{ flex: 1 }}
                   required
                 />
@@ -110,7 +175,17 @@ function App() {
   );
 }
 
-export default App;
+function downloadBlob(blob: Blob, filename: string) {
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style.cssText = "display: none";
+  a.href = url;
+  a.download = filename;
+  a.click();
+  window.URL.revokeObjectURL(url);
+  a.remove();
+}
 
 // thanks so much - https://stackoverflow.com/a/24391376/24182996
 function encodeUTF16LE(str: string) {
